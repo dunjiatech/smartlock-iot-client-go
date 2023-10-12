16ee6a1a50ba4b93b3d331cd5b4ad5d0cbc39820
chenxj <chenxiaojun@dunjiatech.com>
2023-10-12 22:29:51 +0800

    实现设置临时密码接口

--- client/tmpPwd.go
+++ client/tmpPwd.go
@@ -1,9 +1,41 @@
 package client
 
-// type struct {
-// 	Data:
-// }
+type TmpPwdTimeLimit struct {
+	Date    [2][3]int `json:"date"`
+	Minutes [2]int    `json:"minutes"`
+	WeekMap int       `json:"week_map"`
+}
 
-func (c *Client) SetTmpPwd1(pwd string, cntLimit int) bool {
-	return false
+func (c *Client) SetTmpPwd1(productKey string, deviceName string, pwd string, cntLimit int, tmLimit *TmpPwdTimeLimit) bool {
+	log := c.log
+	log.Trace("SetTmpPwd1. ")
+
+	desired := struct {
+		Pwd      string      `json:"prh_tmp_pwd_1"`
+		TmLimit  interface{} `json:"prh_tmp_pwd_tm_limit_1"`
+		CntLimit int         `json:"prh_tmp_pwd_cnt_limit_1"`
+	}{
+		Pwd:      pwd,
+		TmLimit:  tmLimit,
+		CntLimit: cntLimit,
+	}
+
+	return c.IotClient.UpdateDeviceShadowEx(productKey, deviceName, desired, true)
+}
+
+func (c *Client) SetTmpPwd2(productKey string, deviceName string, pwd string, cntLimit int, tmLimit *TmpPwdTimeLimit) bool {
+	log := c.log
+	log.Trace("SetTmpPwd2. ")
+
+	desired := struct {
+		Pwd      string      `json:"prh_tmp_pwd_2"`
+		TmLimit  interface{} `json:"prh_tmp_pwd_tm_limit_2"`
+		CntLimit int         `json:"prh_tmp_pwd_cnt_limit_2"`
+	}{
+		Pwd:      pwd,
+		TmLimit:  tmLimit,
+		CntLimit: cntLimit,
+	}
+
+	return c.IotClient.UpdateDeviceShadowEx(productKey, deviceName, desired, true)
 }
