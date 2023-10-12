c13e9e56f69a3ad79bed5e1066f3c4f00362af13
chenxj <chenxiaojun@dunjiatech.com>
2023-10-12 19:23:23 +0800

    实现设置管理员密码

--- client/client.go
+++ client/client.go
@@ -42,11 +42,24 @@ func (c *Client) SetOpenMode(productKey *string, deviceName *string, mode int) b
 	log := c.log
 	log.Trace("SetOpenMode mode : ", mode)
 
-	m := struct {
+	desired := struct {
 		OpenMode int `json:"open_mode"`
 	}{
 		OpenMode: mode,
 	}
 
-	return c.IotClient.UpdateDeviceShadowEx(productKey, deviceName, m, true)
+	return c.IotClient.UpdateDeviceShadowEx(productKey, deviceName, desired, true)
+}
+
+func (c *Client) SetAdminPassword(productKey *string, deviceName *string, passsword string) bool {
+	log := c.log
+	log.Trace("SetAdminPassword. ")
+
+	desired := struct {
+		Password string `json:"prh_admin_pwd_1"`
+	}{
+		Password: passsword,
+	}
+
+	return c.IotClient.UpdateDeviceShadowEx(productKey, deviceName, desired, true)
 }
