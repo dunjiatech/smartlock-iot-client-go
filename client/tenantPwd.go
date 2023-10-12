dc25820dbefb28a1d0a2da2debd25d4c80fb7b64
chenxj <chenxiaojun@dunjiatech.com>
2023-10-12 19:57:44 +0800

    实现清除租户密码接口

--- client/tenantPwd.go
+++ client/tenantPwd.go
@@ -25,3 +25,18 @@ func (c *Client) SetTenantPwd2(productKey *string, deviceName *string, passsword
 
 	return c.IotClient.UpdateDeviceShadowEx(productKey, deviceName, desired, true)
 }
+
+func (c *Client) ClearAllTenantPwd(productKey *string, deviceName *string) bool {
+	log := c.log
+	log.Trace("ClearAllTenantPwd. ")
+
+	desired := struct {
+		Pwd1 string `json:"prh_tenant_pwd_1"`
+		Pwd2 string `json:"prh_tenant_pwd_2"`
+	}{
+		Pwd1: "",
+		Pwd2: "",
+	}
+
+	return c.IotClient.UpdateDeviceShadowEx(productKey, deviceName, desired, true)
+}
