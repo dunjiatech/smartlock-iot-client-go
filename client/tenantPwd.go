5e56a9d1858c816be52b073487c25ccac46232eb
chenxj <chenxiaojun@dunjiatech.com>
2023-10-12 19:52:42 +0800

    修改设置租户密码接口

--- client/tenantPwd.go
+++ client/tenantPwd.go
@@ -1,31 +1,27 @@
 package client
 
-type TenantPwd1 struct {
-	Pwd string `json:"prh_tenant_pwd_1"`
-}
-
-type TenantPwd2 struct {
-	Pwd string `json:"prh_tenant_pwd_2"`
-}
-
-type TenantPwd3 struct {
-	Pwd string `json:"prh_tenant_pwd_3"`
-}
-
-func (c *Client) SetTenantPassword(productKey *string, deviceName *string, index int, passsword string) bool {
+func (c *Client) SetTenantPwd1(productKey *string, deviceName *string, passsword string) bool {
 	log := c.log
-	log.Trace("SetTenantPassword. ")
+	log.Trace("SetTenantPassword1. ")
 
-	switch index {
-	case 1:
-		return c.IotClient.UpdateDeviceShadowEx(productKey, deviceName, TenantPwd1{Pwd: passsword}, true)
-	case 2:
-		return c.IotClient.UpdateDeviceShadowEx(productKey, deviceName, TenantPwd2{Pwd: passsword}, true)
-	case 3:
-		return c.IotClient.UpdateDeviceShadowEx(productKey, deviceName, TenantPwd3{Pwd: passsword}, true)
-	default:
-		log.Warn("Unknow Tenant index : ", index)
+	desired := struct {
+		Password string `json:"prh_tenant_pwd_1"`
+	}{
+		Password: passsword,
 	}
 
-	return false
+	return c.IotClient.UpdateDeviceShadowEx(productKey, deviceName, desired, true)
+}
+
+func (c *Client) SetTenantPwd2(productKey *string, deviceName *string, passsword string) bool {
+	log := c.log
+	log.Trace("SetTenantPassword2. ")
+
+	desired := struct {
+		Password string `json:"prh_tenant_pwd_2"`
+	}{
+		Password: passsword,
+	}
+
+	return c.IotClient.UpdateDeviceShadowEx(productKey, deviceName, desired, true)
 }
