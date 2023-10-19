cca55ad75a230a2d105c2eaf1a0cd84fa4ff6c48
chenxj <chenxiaojun@dunjiatech.com>
2023-10-19 11:39:05 +0800

    设置管理员密码校验密码是否合法

--- client/admin.go
+++ client/admin.go
@@ -1,13 +1,20 @@
 package client
 
-func (c *Client) SetAdminPassword(productKey string, deviceName string, passsword string) (bool, error) {
+import "fmt"
+
+func (c *Client) SetAdminPassword(productKey string, deviceName string, password string) (bool, error) {
 	log := c.log
 	log.Trace("SetAdminPassword. ")
+
+	if !isPwdLegal(password) {
+		log.Warnf("SetAdminPassword password illegality. password : %s", password)
+		return false, fmt.Errorf("illegality password")
+	}
 
 	desired := struct {
 		Pwd string `json:"prh_admin_pwd_1"`
 	}{
-		Pwd: passsword,
+		Pwd: password,
 	}
 
 	return c.IotClient.UpdateDeviceShadowEx(productKey, deviceName, desired, true)
